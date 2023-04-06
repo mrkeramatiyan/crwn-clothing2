2c3df7c5f770a6ffcf0f9688b64348d6ac88e08d
mrkeramatiyan <70322925+mrkeramatiyan@users.noreply.github.com>
2023-04-06 18:05:08 +0430

    created checkout page along with checkout items and checkout cart functionality

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,23 +1,32 @@
 import { useContext } from "react";
+import { useNavigate } from "react-router";
 
 import { CartContext } from "../../contexts/cart.context";
 
 import Button from "../button/button.component";
-import CartItem from '../cart-item/cart-item.component';
+import CartItem from "../cart-item/cart-item.component";
 
 import "./cart-dropdown.styles.scss";
 
 const CartDropdown = () => {
-  const {cartItems} = useContext(CartContext);
-  
+  const { cartItems } = useContext(CartContext);
+  const navigate = useNavigate();
+
+  const handleClick = () => {
+    navigate('/checkout');
+  };
+
   return (
-    <div className="cart-dropdown-container" onClick={(event) => event.stopPropagation()}>
+    <div
+      className="cart-dropdown-container"
+      onClick={(event) => event.stopPropagation()}
+    >
       <div className="cart-items">
-        {
-          cartItems.map(item => <CartItem key={item.id} cartItem={item} />)
-        }
+        {cartItems.map((item) => (
+          <CartItem key={item.id} cartItem={item} />
+        ))}
       </div>
-      <Button>GO TO CHECKOUT</Button>
+      <Button onClick={handleClick}>GO TO CHECKOUT</Button>
     </div>
   );
 };
